b08bb7bddd3ae79da8d2794ce1c67a45d13be4a3
qinjo <60745301+qinjo@users.noreply.github.com>
2021-12-03 11:06:34 +0800

    add favicon ico

--- qinjo-blog-fe/vue.config.js
+++ qinjo-blog-fe/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  outputDir: 'docs'
+  outputDir: 'docs',
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      favicon48: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTitleImagr: 'favicon.ico'
+    }
+  }
 }
