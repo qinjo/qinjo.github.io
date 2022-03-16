ff187644fd1aafd2d0c4234699f059b9b743c0f7
qinjo <60745301+qinjo@users.noreply.github.com>
2022-03-16 17:42:02 +0800

    add font

--- qinjo-blog-fe/src/pages/modules/home.vue
+++ qinjo-blog-fe/src/pages/modules/home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
     <div class="left">
-      <h1>JO design</h1>
-      <h2>hi, 各位好, 以下既是我的身份也是我的标签</h2>
+      <h1>JO DESIGN</h1>
+      <h2>HI, 各位好, 以下既是我的身份也是我的标签</h2>
       <ul>
-        <li>jo姐老公</li>
+        <li>JO姐老公</li>
         <li>嘟嘟爸爸</li>
         <li>梦想成为自由设计师</li>
+        <li>动漫迷</li>
         <li>熟悉小小前端</li>
         <li>曾经的篮球迷</li>
-        <li>动漫迷</li>
         <li>小摄影迷</li>
         <li>周董迷</li>
         <li>...</li>
